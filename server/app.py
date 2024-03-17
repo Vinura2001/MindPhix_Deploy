34bde98011f85075fa3e1926dc63e1a60d45d99f
Wanninayaka Mudiyanselaage Vinura Imesh <w.m.v.imesh02@gmail.com>
2024-03-18 01:01:14 +0530

    jj

--- server/app.py
+++ server/app.py
@@ -4,13 +4,10 @@ from sklearn.preprocessing import StandardScaler
 import joblib as joblib
 import os
 
-model=joblib.load('iris_model_LR.pkl')
-scaler=joblib.load('scaler.save')
+
 
 app =Flask(__name__)
 
-IMG_FOLDER=os.path.join('static','IMG')
-app.config['UPLOAD_FOLDER']=IMG_FOLDER
 
 
 @app.route('/')
