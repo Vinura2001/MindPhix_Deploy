83958049518d6a769f1843ae82579416a4d0a8ee
Wanninayaka Mudiyanselaage Vinura Imesh <w.m.v.imesh02@gmail.com>
2024-03-18 01:24:02 +0530

    hh

--- server/app.py
+++ server/app.py
@@ -4,10 +4,10 @@ from sklearn.preprocessing import StandardScaler
 import joblib as joblib
 import os
 
-model=joblib.load('model.joblib')
+model=joblib.load('iris_model_LR.pkl')
+scaler=joblib.load('scaler.save')
 
 app =Flask(__name__)
-
 
 
 @app.route('/')
