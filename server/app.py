e8250307b389d2515a2b4234fa04a843f06faf4f
Wanninayaka Mudiyanselaage Vinura Imesh <w.m.v.imesh02@gmail.com>
2024-03-18 00:56:21 +0530

    jj

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request, jsonify
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import joblib as joblib
